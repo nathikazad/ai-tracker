d621ae7e2b5e57b6f650ef545ea0f579fec53c02
Nathik <nathikazd@gmail.com>
2024-04-05 14:45:01 -0700

    jwt fux?

--- server/src/resources/apple.ts
+++ server/src/resources/apple.ts
@@ -48,8 +48,8 @@ function generateJWT(userId: number) {
         "iat": Math.floor(Date.now() / 1000),
         "https://hasura.io/jwt/claims": {
             "x-hasura-default-role": "user",
-            // "x-hasura-allowed-roles": ["user"],
-            "x-hasura-user-id": userId,
+            "x-hasura-allowed-roles": ["user"],
+            "x-hasura-user-id": 1,
         }
     };
 
