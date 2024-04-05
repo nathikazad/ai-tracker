b89fdef88c9fbe9350e7a1244acd946e5983baed
Nathik <nathikazd@gmail.com>
2024-04-05 09:24:13 -0700

    added public key to docker file

---
+++ ios/History/History/Models/Message.swift
@@ -0,0 +1,8 @@
+//
+//  Message.swift
+//  History
+//
+//  Created by Nathik Azad on 4/5/24.
+//
+
+import Foundation
---
+++ ios/History/History/Views/MessageView.swift
@@ -0,0 +1,8 @@
+//
+//  MessageView.swift
+//  History
+//
+//  Created by Nathik Azad on 4/5/24.
+//
+
+import Foundation
