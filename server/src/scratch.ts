4c9431b739415b1f7be5967e246c01602db6d327
Nathik <nathikazd@gmail.com>
2024-04-23 15:37:59 -0700

    compiler watnings

--- server/src/scratch.ts
+++ server/src/scratch.ts
@@ -1,26 +1,26 @@
 // import { parseGoal } from "./resources/logic/goalLogic";
 
-import { StopMovementRequest, processMovement } from "./helper/location"
+// import { StopMovementRequest, processMovement } from "./helper/location"
 
 // parseGoal("Drink 1.5 liters of water everyday", 3)
 // parseGoal("Practice french everyday for 30 minutes", 3)
 // parseGoal("Wake up at 6:30 am everyday", 3)
 
 
-async function main() {
-    let stopMovementRequest: StopMovementRequest = {
-        eventType: 'stoppedMoving',
-        numberOfPoints: 1,
-        timeSinceLastMovement: 10.063171029090881,
-        locations: [ {
-            lat: 37.792811065941684,
-            lon: -122.40529245967804,
-            accuracy: 10,
-            timestamp: '2024-04-18T18:23:45.840Z'
-        }]
-    }
-    await processMovement(1, stopMovementRequest)
+// async function main() {
+//     let stopMovementRequest: StopMovementRequest = {
+//         eventType: 'stoppedMoving',
+//         numberOfPoints: 1,
+//         timeSinceLastMovement: 10.063171029090881,
+//         locations: [ {
+//             lat: 37.792811065941684,
+//             lon: -122.40529245967804,
+//             accuracy: 10,
+//             timestamp: '2024-04-18T18:23:45.840Z'
+//         }]
+//     }
+//     await processMovement(1, stopMovementRequest)
     
-}
-main()
+// }
+// main()
 
