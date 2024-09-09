180d8610363c57b434a24d180cdf44e543205b7e
nathikazad <nathikazad@gmail.com>
2024-09-09 16:25:06 -0700

    added desktop app sign key

--- server/src/resources/authorization.ts
+++ server/src/resources/authorization.ts
@@ -49,7 +49,7 @@ export function verifyAppleJwt(token: string): Promise<jwt.JwtPayload | undefine
 
 export async function convertAppleJWTtoHasuraJWT(appleJWT: string, username: string | null, language: string | null) {
     const decoded = await verifyAppleJwt(appleJWT)
-    if (decoded!["aud"] == 'com.snow.aspire') {
+    if (decoded!["aud"] == 'com.snow.aspire' || decoded!["aud"] == 'com.snow.aspire.Aspire-Mac' ) {
         const userId = await getHasuraUserId(decoded, username, language);
         const token = generateJWT(userId);
         return token;
